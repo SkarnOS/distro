8a785553b2e47f697b7eeb7cb04c63a368b1644c
magic_rb <richard@numtide.com>
2026-01-22 15:09:39 +0100

    And then there was the beginning
    Signed-off-by: magic_rb <richard@numtide.com>

---
+++ dev-shell.nix
@@ -0,0 +1,11 @@
+{
+  perSystem =
+    { config, pkgs, ... }:
+    {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          config.treefmt.build.wrapper
+        ];
+      };
+    };
+}
