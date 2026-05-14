7189ef046edba26c9a3e4cb18eafb6212d242729
magic_rb <richard@numtide.com>
2026-05-14 19:24:37 +0200

    Fix `kudadm` config building

--- modules/nixos/kubernetes/default.nix
+++ modules/nixos/kubernetes/default.nix
@@ -91,8 +91,10 @@ let
     apiVersion: kubelet.config.k8s.io/v1beta1
     kind: KubeletConfiguration
     serverTLSBootstrap: true
-    ---
-    ${lib.join "\n---\n" cfg.kubeadmExtraConfig}
+    ${lib.optionalString (cfg.kubeadmExtraConfig != [ ]) ''
+      ---
+      ${lib.join "\n---\n" cfg.kubeadmExtraConfig}
+    ''}
   '';
 in
 {
